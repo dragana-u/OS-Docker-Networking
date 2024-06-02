6a7227d067cbd0fa190d7b12cbfdb57149520ec1
dragana-u <draganausovic2@gmail.com>
2024-06-02 14:29:42 +0200

    Auditory exercises

---
+++ src/Auditoriski/ex1/HelloWorld.java
@@ -0,0 +1,7 @@
+//package Auditoriski.ex1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Docker!");
+    }
+}
---
+++ src/Auditoriski/ex3/backend/HttpServ.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+public class HttpServ {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new java.net.InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        System.out.println("Server is running on port 8080...");
+    }
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello from Java HTTP server!";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
